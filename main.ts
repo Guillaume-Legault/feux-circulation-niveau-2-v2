e8556a1985d20c1f64cb615fde1e154a137886ae
Guillaume-Legault <93203429+Guillaume-Legault@users.noreply.github.com>
2021-11-26 15:45:25 -0500

    Mise à jour pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -21,23 +21,32 @@ function Cycle_lumière () {
     debut_cycle_lumiere = 1
     pins.digitalWritePin(DigitalPin.P8, 1)
     pins.digitalWritePin(DigitalPin.P0, 1)
-    basic.pause(5000)
+    basic.pause(15000)
     pins.digitalWritePin(DigitalPin.P0, 0)
     pins.digitalWritePin(DigitalPin.P1, 1)
-    basic.pause(2000)
+    basic.pause(5000)
     pins.digitalWritePin(DigitalPin.P1, 0)
     pins.digitalWritePin(DigitalPin.P2, 1)
-    basic.pause(5000)
+    basic.pause(10000)
     pins.digitalWritePin(DigitalPin.P2, 0)
     debut_cycle_lumiere = 0
     basic.pause(100)
 }
+let temps = 0
 let debut_cycle_pieton = 0
 let debut_cycle_lumiere = 0
 let Pieton = 0
 Pieton = 0
 debut_cycle_lumiere = 0
 debut_cycle_pieton = 0
+basic.forever(function () {
+    if (debut_cycle_pieton == 1) {
+        for (let index = 0; index <= 10; index++) {
+            temps = index * -1 + 10
+            basic.showNumber(temps)
+        }
+    }
+})
 basic.forever(function () {
     if (Pieton == 1 && debut_cycle_lumiere == 0) {
         Cycle_pieton()
